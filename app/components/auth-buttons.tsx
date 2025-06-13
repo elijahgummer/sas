6e2e5e5b58cbe63052f651a4bf175e3e26499aac
Elijah Gummer <elijahgummer5@gmail.com>
2025-06-13 13:55:36 +1000

    rechanged the directory

--- app/components/auth-buttons.tsx
+++ app/components/auth-buttons.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Link from "next/link"
-import { Button } from "@/components/ui/button"
+import { Button } from "./ui/button"
 import { LogIn } from "lucide-react"
 
 export function AuthButtons() {
