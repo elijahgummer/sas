4bdde022a37e4690ea8ddcf0469b9113c5509185
Elijah Gummer <elijahgummer5@gmail.com>
2025-06-15 09:34:18 +1000

    changed the code

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,12 @@ import Features from './components/Features';
 import Pricing from './components/Pricing';
 import CTA from './components/CTA';
 import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 
 export default function Home() {
   return (
     <>
+      <Navbar />
       <ResumeAnalyzerApp />
       <Hero resumeScore={85} />
       <Builder />
