06767866a407c16df446fdc12140e4cb4ee990b6
Elijah Gummer <elijahgummer5@gmail.com>
2025-06-14 12:26:53 +1000

    added tyhe builder section

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,15 @@
 
 import ResumeAnalyzerApp from './components/ResumeAnalyzerApp';
 import Hero from "./components/Hero"
+import Builder from './components/Builder';
 
 export default function Home() {
   return (
     <>
       <ResumeAnalyzerApp />
       <Hero resumeScore={85} />
+      <Builder />
+      
     </>
   ) 
 }
